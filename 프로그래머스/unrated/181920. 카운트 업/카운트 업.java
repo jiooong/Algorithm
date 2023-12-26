753c1863576f4f205da4a39537e27dbcd543a88e
jiooong <lucy991010@gmail.com>
2023-12-26 22:13:54 +0900

    [level unrated] Title: 카운트 업, Time: 0.04 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
@@ -3,18 +3,18 @@ class Solution {
     
     public int[] solution(int start_num, int end_num) {
         ArrayList<Integer> arrList = new ArrayList<>();
-
-        for(int i=start_num; i<=end_num; i++){
+        
+        for(int i = start_num; i<=end_num; i++){
             arrList.add(i);
-            
         }
         
-        int[] resultArray = new int[arrList.size()];
-        for(int i=0; i<resultArray.length; i++){
-            resultArray[i] = arrList.get(i);
+        int[] arr = new int[arrList.size()];
+        int size = 0;
+        for(int j : arrList){
+            arr[size++] = j;
         }
         
         
-        return resultArray;
+        return arr;
     }
 }
