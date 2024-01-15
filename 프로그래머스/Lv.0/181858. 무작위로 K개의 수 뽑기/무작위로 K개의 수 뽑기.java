f30d5d82d01fd3be563e9350d9c3e0c5785d4060
jiooong <lucy991010@gmail.com>
2024-01-16 00:22:39 +0900

    [level Lv.0] Title: 무작위로 K개의 수 뽑기, Time: 165.14 ms, Memory: 83.8 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+class Solution {
+    public int[] solution(int[] arr, int k) {
+        List<Integer> list = new ArrayList<>();
+        for(int i : arr){
+            if(!list.contains(i)){
+                list.add(i);
+            }
+        }
+
+        int remaining = k - list.size();
+        for (int i = 0; i < remaining; i++) {
+            list.add(-1);
+        }
+        
+        return list.subList(0,k).stream().mapToInt(i->i).toArray();
+    }
+}
