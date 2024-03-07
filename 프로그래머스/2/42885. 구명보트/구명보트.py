9d39f7720c6214881e438946f034d45784771954
jiooong <lucy991010@gmail.com>
2024-03-07 23:53:53 +0900

    [level 2] Title: 구명보트, Time: 11.34 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -0,0 +1,22 @@
+from collections import deque
+def solution(people, limit):
+    answer = 0
+    people.sort()
+    people = deque(people)
+    
+    while people:
+        if len(people) == 1:
+            answer += 1
+            break
+        elif people[0] + people[-1] <= limit:
+            answer += 1
+            people.pop()
+            people.popleft()
+            
+        else:
+            answer +=1
+            people.pop()
+
+
+
+    return answer
