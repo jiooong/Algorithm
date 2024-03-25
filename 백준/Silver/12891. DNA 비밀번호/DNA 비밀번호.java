521977c90077d20c3e61492cddf89b7a7f8f65ad
jiooong <lucy991010@gmail.com>
2024-03-26 01:46:58 +0900

    [Silver II] Title: DNA 비밀번호, Time: 724 ms, Memory: 107744 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -1,45 +1,54 @@
+import com.sun.source.tree.Tree;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
 
 public class Main {
     static int answer;
-    static String[] DNA = {"A", "C", "G", "T"};
+    static String[] s = {"A", "C", "G", "T"};
+    public static void checking(Map<String, Integer> map, Map<String, Integer> count){
+        for(int i=0; i<4; i++){
+            if(map.get(s[i]) > count.get(s[i])){
+                return;
+            }
+        }
+            answer ++;
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().split(" ");
+        int S = Integer.parseInt(str[0]);
+        int P = Integer.parseInt(str[1]);
+
+        String[] dnastr = br.readLine().split("");
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int S = Integer.parseInt(st.nextToken());
-        int P = Integer.parseInt(st.nextToken());
+        Map<String, Integer> map = new HashMap<>(); // 있어야 하는 값
+        Map<String, Integer> count = new HashMap<>(); // 확인하려고 저장하는 값
 
-        Map<String, Integer> check = new HashMap<>();
-        Map<String, Integer> current = new HashMap<>();
-        String[] str = br.readLine().split("");
-
-        st = new StringTokenizer(br.readLine());
-        for(String s : DNA) {
-            check.put(s, Integer.parseInt(st.nextToken()));
-            current.put(s, 0);
+        for(String ss : s) {
+            map.put(ss, Integer.parseInt(st.nextToken()));
+            count.put(ss, 0);
         }
 
-        for(int i = 0; i < P; i++)
-            current.put(str[i], current.get(str[i]) + 1);
-        valid(current, check);
-
-        for(int i = 1; i <= str.length - P; i++) {
-            current.put(str[i - 1], current.get(str[i - 1]) - 1);
-            current.put(str[i + P - 1], current.get(str[i + P - 1]) + 1);
-            valid(current, check);
+        for(int i=0; i<P; i++){
+            count.put(dnastr[i], count.get(dnastr[i]) + 1);
         }
+        checking(map,count);
+
+        for(int i=1; i<=S-P; i++){
+            count.put(dnastr[i-1], count.get(dnastr[i-1]) - 1); // 이전의 값은 - 1 빼주고
+            count.put(dnastr[i+P-1], count.get(dnastr[i+P-1]) + 1); // 다음 값은  + 1 넣허줘
+            checking(map,count);
+        }
+
+
         System.out.println(answer);
-        br.close();
-    }
-
-    private static void valid(Map<String, Integer> current, Map<String, Integer> check) {
-        for(String s : DNA) {
-            if(current.get(s) < check.get(s))
-                return;
-        }
-        answer++;
     }
 }
