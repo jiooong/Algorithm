c341847e829bac2317dcc7e4134ecfe19d24b2c7
jiooong <lucy991010@gmail.com>
2024-04-11 21:06:17 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 1.35 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/README.md
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 가운데 글자 가져오기 - 12903 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12903?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12903) 
 
 ### 성능 요약
 
-메모리: 73.3 MB, 시간: 1.37 ms
+메모리: 73.7 MB, 시간: 1.35 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 4일 0:46:44
+2024년 04월 11일 21:06:13
 
 ### 문제 설명
 
--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -1,10 +1,10 @@
 class Solution {
     public String solution(String s) {
         String answer = "";
-        if(s.length()%2==0){
-            answer+= s.substring(s.length()/2 -1,s.length()/2+1);
+        if(s.length()%2==1){
+            answer += s.charAt(s.length()/2);
         }else{
-            answer += s.charAt(s.length()/2) +"";
+            answer += s.substring((s.length()/2)-1,s.length()/2+1);
         }
         return answer;
     }
