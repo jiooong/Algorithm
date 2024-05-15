df4547883fc52462270b903a51831430517d2203
jiooong <lucy991010@gmail.com>
2024-05-15 23:58:54 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -1,7 +1,7 @@
 SELECT ID, EMAIL, FIRST_NAME, LAST_NAME
 FROM DEVELOPERS A
 WHERE SKILL_CODE & (SELECT SUM(CODE)
-                                        FROM SKILLCODES
-                                           WHERE CATEGORY = 'Front End'
+                    FROM SKILLCODES
+                    WHERE CATEGORY = 'Front End'
                                         ) 
-ORDER BY ID 
+ORDER BY ID
