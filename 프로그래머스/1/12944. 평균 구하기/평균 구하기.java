5697b406eb41f5b223be51b043de0d84daa6c2bc
jiooong <lucy991010@gmail.com>
2024-09-10 01:01:10 +0900

    [level 1] Title: 평균 구하기, Time: 0.06 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -1,9 +1,10 @@
 class Solution {
     public double solution(int[] arr) {
         double answer = 0;
-        for(int i : arr){
-            answer += i;
+        for(int i =0; i<arr.length; i++){
+            answer += arr[i];
         }
         return answer/arr.length;
+
     }
 }
